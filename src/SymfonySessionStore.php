fa84b8b901d211381443b33f970976d4058d5491
Honore Hounwanou <mercuryseries@gmail.com>
2021-05-07 18:16:27 -0400

    Make use of the request_stack service instead of the deprecated session service

--- src/SymfonySessionStore.php
+++ src/SymfonySessionStore.php
@@ -2,15 +2,15 @@
 
 namespace MercurySeries\FlashyBundle;
 
-use Symfony\Component\HttpFoundation\Session\Session;
+use Symfony\Component\HttpFoundation\RequestStack;
 
 class SymfonySessionStore implements SessionStore
 {
     private $session;
 
-    public function __construct(Session $session)
+    public function __construct(RequestStack $requestStack)
     {
-        $this->session = $session;
+        $this->session = $requestStack->getSession();
     }
 
     public function flash(string $name, array $data): void
